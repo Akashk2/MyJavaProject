1aa33f1f4ffaa9ad12da70a6b97b6d934f9d5585
Akashk2 <50747122+Akashk2@users.noreply.github.com>
2021-12-14 10:44:30 +0530

    This is my first java project with github

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 System.out.println("Hello Everyone");
-System.out.println("Good Morning");		
+System.out.println("Good Morning");	
+System.out.println("How are u guys");
 }
 }
